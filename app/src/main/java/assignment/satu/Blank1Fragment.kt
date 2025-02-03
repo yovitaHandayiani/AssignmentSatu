5bf5f447b8fa09f215b03d30bd8dbafe92cec5a5
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2025-02-03 17:31:02 +0700

    21:45

--- app/src/main/java/assignment/satu/Blank1Fragment.kt
+++ app/src/main/java/assignment/satu/Blank1Fragment.kt
@@ -5,14 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import assignment.satu.databinding.FragmentBlank1Binding
 
 
 class Blank1Fragment : Fragment() {
+    private lateinit var binding: FragmentBlank1Binding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank1, container, false)
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_blank1, container, false)
+        binding = FragmentBlank1Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnNext.setOnClickListener {
+            val frament2 = Blank2Fragment()
+            val bundle2 = Bundle()
+            bundle2.putString(Blank2Fragment.EXTRA_VALUE, "value")
+            frament2.arguments = bundle2
+
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager
+                .beginTransaction().apply {
+                     replace(R.id.fragment_container, frament2, Blank2Fragment::class.java.simpleName)
+                    addToBackStack(null)
+                    commit()
+                }
+        }
     }
 }
--- app/src/main/java/assignment/satu/Blank2Fragment.kt
+++ app/src/main/java/assignment/satu/Blank2Fragment.kt
@@ -5,14 +5,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import assignment.satu.databinding.FragmentBlank1Binding
+import assignment.satu.databinding.FragmentBlank2Binding
 
 
 class Blank2Fragment : Fragment() {
+    companion object {
+        var EXTRA_VALUE = "extra_value"
+    }
+
+    private lateinit var binding: FragmentBlank2Binding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+//        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        binding = FragmentBlank2Binding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (arguments != null) {
+            val label = arguments?.getString(EXTRA_VALUE)
+            binding.myText.text = label
+        }
+
+        binding.btnBack.setOnClickListener {
+            val frament1 = Blank1Fragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager
+                .beginTransaction().apply {
+                    replace(
+                        R.id.fragment_container,
+                        frament1,
+                        Blank1Fragment::class.java.simpleName
+                    )
+                    addToBackStack(null)
+                    commit()
+                }
+        }
     }
 }
